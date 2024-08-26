01a69ff868452f4c43e65992d8e3b892fea3bb4f
SuitOne <a.scesny@att.net>
2024-08-25 23:47:59 -0400

    Created TODO.md
    Moved the TODO list from the main cpp file to a dedicated md file

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO:
+- Edit success message to show how many files were actually successfully encrypted/decrypted
+- Add recursiveness to file encryption
+- Check that files to decrypt have .ses extension before attempting decryption
+- Append .ses files with seed before encryption to ensure the data retention during decryption
+- Revamp user interaction with program
+- Add user ability to change seed length
+- Add updater that checks github for newer releases
--- src/Encryptor.cpp
+++ src/Encryptor.cpp
@@ -1,12 +1,4 @@
 ﻿// Encryptor.cpp : Defines the entry point for the application.
-// TODO:
-// Edit success message to show how many files were actually successfully encrypted/decrypted
-// Add recursiveness to file encryption
-// Check that files to decrypt have .ses extension before attempting decryption
-// Append .ses files with seed before encryption to ensure the data retention during decryption
-// Revamp user interaction with program
-// Add user ability to change seed length
-// Add updater that checks github for newer releases
 
 #include "Encryptor.h"
 
